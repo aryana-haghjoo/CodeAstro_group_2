069bfb7a674bf0850ebcf3ea095bbf9379de707a
Michele <mnwooddog@gmail.com>
2026-06-22 13:31:14 -0700

    make file to add functions to, open spectrum

---
+++ utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import astropy.io.fits as fits
+
+def open_spectrum(file):
+    """
+    Opens a spectrum file and returns the wavelength and intensity arrays.
+    
+    Returns:
+        wavelength (numpy.ndarray): Array of wavelengths.
+        intensity (numpy.ndarray): Array of intensities.
+    """
+    spec = fits.open(file)
+    wavelength = spec[0].data['WAVELENGTH']
+    intensity = spec[0].data['FLUX']
+    
+    return wavelength, intensity
